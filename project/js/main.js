359eee74f3dab42b1bd4db41763844d5e252da8c
eraser <eraser1991@yandex.ru>
2020-05-26 18:18:27 +0300

    4

--- project/js/main.js
+++ project/js/main.js
@@ -1,19 +1,17 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class ProductsList{
     constructor(container = '.products'){
         this.container = container;
         this.goods = [];
         this.allProducts = [];
-        this._fetchProduct();
-        this.render();
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this.render()
+            });
     }
-    _fetchProduct(){
-        this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ];
-    }
+ 
     
     render(){
         const block = document.querySelector(this.container);
@@ -23,12 +21,16 @@ class ProductsList{
             block.insertAdjacentHTML('beforeend',productObj.render());
         }
     }
-
+    _getProducts(){
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
     goodsSum(){
         console.log(this.goods.reduce(
-           function(p,c){
-               return parseInt(p + c.price);
-                }
+           (p,c) => {return parseInt(p + c.price);}   
         ,''))
     }
     
@@ -36,10 +38,10 @@ class ProductsList{
 
 class ProductItem{
     constructor(product, img="https://placehold.it/200x150"){
-        this.title = product.title;
-        this.id = product.id;
-        this.img = img;
+        this.title = product.product_name;
         this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
     }
     
     render(){
@@ -58,21 +60,55 @@ class ProductItem{
  
 let list = new ProductsList();
 
-list.goodsSum();
+class Basket {
+    constructor(container = '.basket'){
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this.addGoods()
+            });
+    }
+    addGoods() {
+        let buy = document.getElementsByClassName("buy-btn");
+        buy.onclick = () => {
+            const block = document.querySelector(this.container);
+            let elemBask = new elemBasket(product);
+            this.allProducts.push(elemBask);
+            block.insertAdjacentHTML('beforeend',elemBask.render())
+        }
 
-// class basket {
-//     constructor
-//     render(){
+    }
+    removeGoods() {
 
-//     }
-//     goodsSum(){
+    }
+    changeGoods() {
 
-//     }
-// }
+    }
+    render(){
+        let basketBtn = document.getElementsByClassName("btn-cart");
+        basketBtn.onclick = () => {
+            let visible = document.getElementsByClassName("basket");
+            visible.style.visibiliti = 'visible';
+     }
 
-// class basketItem{
-//     constructor
-//     render(){
+    }
+}
 
-//     }
-// }
+class elemBasket {
+    constructor(product){
+        this.title = product.product_name;
+        this.price = product.price;
+    }
+    render(){
+        return `<div class="basket-item">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="delete-good">Удалить товар</button>
+            </div>`
+    }
+}
+
+let basket = new Basket();
