5927e100a3a63f29d1379f0906c2e527097db01b
eraser <eraser1991@yandex.ru>
2020-06-09 22:59:49 +0300

    last

--- project/public/js/ProductComponent.js
+++ project/public/js/ProductComponent.js
@@ -13,8 +13,19 @@ Vue.component('product', {
             <div class="product-item">
                 <img :src="img" alt="Some img">
                 <div class="desc">
-                    <h3>{{product.product_name}}</h3>
-                    <p>{{product.price}}</p>
+                    <div class="product-bio-desc">
+                        <h3>{{product.product_name}}</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                         Ducimus a laudantium placeat dicta aperiam, quis reprehenderit quibusdam eum 
+                         fugit nobis esse labore est delectus soluta porro facilis in praesentium optio
+                          assumenda dolores consequuntur impedit minima? Sit consequatur soluta velit fugit ipsa
+                           accusamus nam ex, consequuntur modi? Exercitationem id illum libero reiciendis eos blanditiis
+                            velit non culpa, consequatur facilis, delectus expedita, tempore maxime. Culpa repellat voluptatum incidunt
+                             officia vel dicta aliquam alias, rerum consequuntur minima nulla? Pariatur voluptatem accusamus dolorum, libero
+                              reiciendis labore minima nostrum recusandae. Eligendi obcaecati non, repellat, culpa voluptatibus ipsa vero architecto
+                               neque repellendus ullam optio ipsum error?</p>
+                        <p>{{product.price}}</p>
+                    </div>
                     <button class="buy-btn" @click="$parent.$emit('add-product', product)">Купить</button>
                 </div>
             </div>
